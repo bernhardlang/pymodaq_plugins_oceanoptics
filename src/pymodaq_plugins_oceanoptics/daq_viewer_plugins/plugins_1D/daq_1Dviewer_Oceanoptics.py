8198ba661bf6e36573ef1c4faa9c4d1ec32190b9
Bernhard Lang <bernhard.lang@unige.ch>
2025-03-26 10:28:20 +0100

    plugin works as standalone and in dashboard

--- src/pymodaq_plugins_oceanoptics/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Oceanoptics.py
+++ src/pymodaq_plugins_oceanoptics/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Oceanoptics.py
@@ -6,15 +6,15 @@ from pymodaq_gui.parameter import Parameter
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, \
     comon_parameters, main
 from pymodaq.utils.data import DataFromPlugins
-import seabreeze
 
-seabreeze.use('cseabreeze')
+import seabreeze
+#seabreeze.use('cseabreeze')
 from seabreeze.spectrometers import Spectrometer, list_devices
 
 # Note: there's no special controller because that job is already done
 # by the seabreeze library.
 
-class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
+class DAQ_1DViewer_Oceanoptics(DAQ_Viewer_base):
     """ Instrument plugin class for a 1D viewer using Ocean Optics' seabreeze
         library.
 
@@ -28,9 +28,12 @@ class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
     integrationtime
     """
 
+    device_names = [str(device) for device in list_devices()]
+    hardware_averaging = True
+
     params = comon_parameters+[
-        { 'title': 'Device:', 'name': 'device', 'type': 'list',
-          'limits': devices },
+        { 'title': 'Device:', 'name': 'device_name', 'type': 'list',
+          'limits': device_names },
         { 'title': 'Integration Time [ms]:', 'name': 'integration',
           'type': 'float', 'value': 1.0 },
         { 'title': 'X-Axis in wavenumbers:', 'name': 'wavenumber',
@@ -40,6 +43,7 @@ class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
     def ini_attributes(self):
         self.controller: Spectrometer = None
         self.x_axis = None
+        self.device_connected = False # see stop method
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -75,12 +79,11 @@ class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
         self.ini_detector_init(slave_controller=controller)
 
         if self.is_master:
-            dvc = self.settings.child('device').value()
-            try:
-                self.controller = Spectrometer(dvc)
-                self.controller.open()
-            except:
-                return "No device, or failure to initialize", False
+            device_name = self.settings.child('device_name').value()
+            device_index = self.device_names.index(device_name)
+            self.controller = Spectrometer(list_devices()[device_index])
+            self.controller.open()
+            self.device_connected = True
 
             self.first_used_pixel = \
                 self.controller.f.spectrometer\
@@ -91,11 +94,13 @@ class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
             dfp = DataFromPlugins(name='Avantes',
                                   data=[np.zeros(len(wavelengths))],
                                   dim='Data1D', axes=[self.x_axis],
-                                  labels=['Avantes-Signal'])
+                                  labels=['Intensities'])
             self.dte_signal_temp.emit(DataToExport(name='Avantes', data=[dfp]))
             tlimits = \
                 np.array(self.controller.integration_time_micros_limits) / 1000
             self.settings.child('integration').setLimits(tlimits)
+            self.controller.close()
+            self.device_connected = False
 
         info = "OcenaOptics Spectrometer initilialized"
         initialized = True
@@ -105,6 +110,7 @@ class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
     def close(self):
         """Terminate the communication protocol"""
         self.controller.close()
+        self.device_connected = False
         self.x_axis = None
 
     def grab_data(self, Naverage=1, **kwargs):
@@ -119,12 +125,16 @@ class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
         kwargs: dict
             others optionals arguments
         """
+        if not self.device_connected: # see stop method
+            self.controller.open()
+            self.device_connected = True
+
         if Naverage > 1:
-            raw_data = [self.controller.intensities()[self.first_used_pixel] \
+            raw_data = [self.controller.intensities()[self.first_used_pixel:] \
                         for i in range(Naverage)]
             data = np.array(raw_data).mean(0)
         else:
-            data = self.controller.intensities(correct_nonlinearity=nlc)[c0:]
+            data = self.controller.intensities()[self.first_used_pixel:]
 
         dfp = DataFromPlugins(name='seabreeze', data=[data], dim='Data1D',
                               labels=['Intensity'], axis=[self.x_axis])
@@ -135,7 +145,13 @@ class DAQ_1DViewer_OceanOptics(DAQ_Viewer_base):
         pass
 
     def stop(self):
-        pass
+        # bugfix, when operating from this script only,
+        # the close methods of the plugin is never called, leaving the
+        # seabreeze library in a dead state. Only unplugging and replugging
+        # the device gets it back to life. So we close communication here
+        # in case the plugin is shut down afer this call.
+        self.controller.close()
+        self.device_connected = False
 
 
 if __name__ == '__main__':
